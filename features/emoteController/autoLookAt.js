6105063aa06e9776f4ed57120f60b7b93dad4ab9
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-05-04 09:10:51 +0800

    Create autoLookAt.js

---
+++ features/emoteController/autoLookAt.js
@@ -0,0 +1,17 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.149.0/+esm";
+
+/**
+ * 目線を制御するクラス
+ *
+ * サッケードはVRMLookAtSmootherの中でやっているので、
+ * より目線を大きく動かしたい場合はここに実装する。
+ */
+class AutoLookAt {
+  // private _lookAtTarget: THREE.Object3D;
+  constructor(vrm, camera) {
+    this._lookAtTarget = new THREE.Object3D();
+    camera.add(this._lookAtTarget);
+
+    if (vrm.lookAt) vrm.lookAt.target = this._lookAtTarget;
+  }
+}
