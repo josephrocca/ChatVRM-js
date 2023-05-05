ed9fca5213e325873fd9213b20b866eaa42c10f3
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-05-05 01:32:30 +0000

    Add Mixamo FBX animation loading

--- features/vrmViewer/model.js
+++ features/vrmViewer/model.js
@@ -1,9 +1,12 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.149.0/+esm";
 import { VRM, VRMLoaderPlugin, VRMUtils } from "https://cdn.jsdelivr.net/npm/@pixiv/three-vrm@1.0.9/+esm";
 import { GLTFLoader } from "../../GLTFLoader/GLTFLoader.js";
+import { loadMixamoAnimation } from '../../mixamo/loadMixamoAnimation.js';
 import { VRMLookAtSmootherLoaderPlugin } from "../../lib/VRMLookAtSmootherLoaderPlugin/VRMLookAtSmootherLoaderPlugin.js";
 import { LipSync } from "../lipSync/lipSync.js";
 import { EmoteController } from "../emoteController/emoteController.js";
+import { loadVRMAnimation } from "../../lib/VRMAnimation/loadVRMAnimation.js";
+
 
 /**
  * 3Dキャラクターを管理するクラス
@@ -53,13 +56,27 @@ export class Model {
    *
    * https://github.com/vrm-c/vrm-specification/blob/master/specification/VRMC_vrm_animation-1.0/README.ja.md
    */
-  async loadAnimation(vrmAnimation) {
+  async loadAnimation(input) {
     const { vrm, mixer } = this;
     if (vrm == null || mixer == null) {
       throw new Error("You have to load VRM first");
     }
 
-    const clip = vrmAnimation.createAnimationClip(vrm);
+    let clip;
+    if(input.expressionTracks) { // <-- crudely detect if it's a VRMAnimation object
+      clip = input.createAnimationClip(vrm);
+    } else if(typeof input === "string") { // <-- it's a URL
+      if(input.endsWith(".vrma")) {
+        const vrma = await loadVRMAnimation(input);
+        if (vrma) {
+          this.loadAnimation(vrma);
+          return;
+        }
+      } else {
+        // assume it's an fbx file
+        clip = await loadMixamoAnimation(input, vrm);
+      }
+    }
     const action = mixer.clipAction(clip);
     action.play();
   }
